716a22937607df194a27a5d352bc0b19ad7432dc
testinglife001 <testing.life001@gmail.com>
2025-11-03 19:00:42 +0600

    final: Configured deployment for Render & Vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,26 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-// export default defineConfig({
-//  plugins: [react()],
-// })
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // ✅ Only affects local dev (npm run dev)
-      '/api': {
-        target: 'http://localhost:5000',
-        // changeOrigin: true,
-      },
+      '/api': { target: 'http://localhost:5000' },
     },
   },
 })
+
 
 
